4193d83c4581e5e0710aacac4bd8a88bde50e80d
Loc Tran <loc.tran@sstechvn.com>
2021-12-10 11:07:17 +0700

    Extracting CSS Into a Separate Bundle With mini-css-extract-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const TerserPlugin = require("terser-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   entry: "./src/index.js",
@@ -20,14 +21,14 @@ module.exports = {
       {
         test: /\.css$/,
         use: [
-          'style-loader',
+          MiniCssExtractPlugin.loader,
           'css-loader'
         ]
       },
       {
         test: /\.scss$/,
         use: [
-          'style-loader',
+          MiniCssExtractPlugin.loader,
           'css-loader',
           'sass-loader'
         ]
@@ -46,6 +47,9 @@ module.exports = {
     ]
   },
   plugins: [
-    new TerserPlugin()
+    new TerserPlugin(),
+    new MiniCssExtractPlugin({
+      filename: 'styles.css'
+    })
   ]
 }
