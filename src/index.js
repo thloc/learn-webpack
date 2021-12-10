296f80684e6548a1b58755d34c96c45ac751d06b
Loc Tran <loc.tran@sstechvn.com>
2021-12-10 11:29:49 +0700

    WIP

--- src/index.js
+++ src/index.js
@@ -2,9 +2,13 @@ import helloWorld from './hello-world.js';
 import addImage from './add-image.js';
 
 import HelloWorldBtn from './component/hello-world-btn/index.js';
+import Heading from './component/heading/index.js';
 
 helloWorld();
 addImage();
+
+const heading = new Heading();
+heading.render();
 
 const helloWorldBtn = new HelloWorldBtn();
 helloWorldBtn.render();
