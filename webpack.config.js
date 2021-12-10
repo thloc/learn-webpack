115a921ed6fb87935d9fb02c4f30a54c14bc0ae0
Loc Tran <loc.tran@sstechvn.com>
2021-12-10 10:59:51 +0700

    Minification Of The Resulting Webpack Bundle

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
@@ -43,5 +44,8 @@ module.exports = {
         }
       }
     ]
-  }
+  },
+  plugins: [
+    new TerserPlugin()
+  ]
 }
