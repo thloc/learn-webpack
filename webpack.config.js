6cdd1efa8f9af70b89b1b8cb45c06422f705a050
Loc Tran <loc.tran@sstechvn.com>
2021-12-10 10:33:12 +0700

    Handling SCSS

--- webpack.config.js
+++ webpack.config.js
@@ -22,6 +22,14 @@ module.exports = {
           'style-loader',
           'css-loader'
         ]
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
       }
     ]
   }
