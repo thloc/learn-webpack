966c0001f3dcb47518229ab7d99a4c93a78580b2
Loc Tran <loc.tran@sstechvn.com>
2022-01-27 09:58:39 +0700

    Customizing Generated HTML Files

--- webpack.config.js
+++ webpack.config.js
@@ -54,6 +54,12 @@ module.exports = {
       filename: 'styles.[contenthash].css'
     }),
     new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin()
+    new HtmlWebpackPlugin({
+      title: 'Hello World',
+      filename: 'subfolder/custom_filename.html',
+      meta: {
+        viewport: 'width=device-width, initial-scale=1'
+      }
+    })
   ]
 }
